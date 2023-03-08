198cadab618a1a332a5c5211c08756b17f5fd727
Serenay <serenay@1001dubai.com>
2023-03-08 17:51:24 +0300

    Initial commit

---
+++ app/src/main/java/com/moonsunapp/quizapp/Question.kt
@@ -0,0 +1,14 @@
+package com.moonsunapp.quizapp
+
+import java.net.IDN
+
+data class Question(
+    val id: Int,
+    val question: String,
+    val image: Int,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    val correctAnswer: Int
+)
